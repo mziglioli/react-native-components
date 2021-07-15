39c88be7e12a92bf44ebb3a6b9834676c6cf53ed
zigliolim <marceloziglioli@rentalcars.com>
2021-07-15 09:02:30 +0100

    feat(test): add test library

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Appbar } from 'react-native-paper';
 
 interface HeaderProps {
-  value: string;
   onBackAction: () => void;
   onMenuAction: () => void;
 }
 
 const Header = ({ onBackAction, onMenuAction }: HeaderProps) => (
-  <Appbar.Header>
+  <Appbar.Header testID="Header">
     <Appbar.BackAction onPress={onBackAction} touchSoundDisabled={false} />
     <Appbar.Content title="Title" />
     <Appbar.Action
