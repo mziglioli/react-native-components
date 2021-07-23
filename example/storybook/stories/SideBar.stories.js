0e3553ef909ac89a8072f95ccb7c377e575ed538
zigliolim <marceloziglioli@rentalcars.com>
2021-07-23 13:50:33 +0100

    feat(storybook): add storybook script in the root

--- example/storybook/stories/SideBar.stories.js
+++ example/storybook/stories/SideBar.stories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { SideBar, buildItems } from '../../../src';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('SideBar', module).add('Default', () => (
-  <SideBar items={buildItems()} navigation={{}} title="test" />
+  <SideBar
+    items={buildItems()}
+    navigation={{
+      navigate: (value) => {
+        console.log('navigate to', value);
+        action('navigate to', value);
+      },
+    }}
+    title="test"
+  />
 ));
